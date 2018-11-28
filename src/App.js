32620bc5151c01c8e0b130b65140b73de358057c
MigueMartelo <miguemartelo1031@gmail.com>
2018-11-28 16:07:45 -0500

    Agregando rutas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 
+// Components
 import Header from './components/Header';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <React.Fragment>
-          <Header />
-        </React.Fragment>
+          <Router>
+              <React.Fragment>
+                  <Header />
+
+                  <div className="container">
+                      <Switch>
+                          <Route exact path="/" component={Productos}/>
+                          <Route exact path="/productos/nuevo" component={NuevoProducto}/>
+                          <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+                      </Switch>
+                  </div>
+              </React.Fragment>
+          </Router>
       </Provider>
     );
   }
