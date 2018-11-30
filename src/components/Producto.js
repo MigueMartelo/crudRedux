b80ab57ef0378ed570d52cadaed83a1f77ffea47
MigueMartelo <miguemartelo1031@gmail.com>
2018-11-30 09:02:40 -0500

    Mostrando los productos en la vista

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Producto extends Component {
     render() {
+        const {id, nombre, precio} = this.props.info;
         return (
-            <h2>Producto</h2>
+            <li className="list-group-item">
+                <div className="row justify-content-between align-items-center">
+                    <div className="col-md-8 d-flex justify-content-between align-items-center">
+                        <p className="text-dark m-0">{nombre}</p>
+                        <span className="badge badge-warning text-dark">$ {precio}</span>
+                    </div>
+                    <div className="col-md-4 d-flex fustify-content-end acciones">
+                        <Link to={`productos/editar/${id}`} className="btn btn-primary mr-2">Editar</Link>
+                        <button type="button" className="btn btn-danger">Eliminar</button>
+                    </div>
+                </div>
+            </li>
         );
     }
 }
